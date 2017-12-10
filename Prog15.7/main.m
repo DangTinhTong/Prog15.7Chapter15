b1c61f6c4cf5a03b6554b1525aa20c33ceb4deb6
admin <dangtinhtong@gmail.com>
2017-12-10 07:25:00 +0700

    First commit

--- Prog15.7/main.m
+++ Prog15.7/main.m
@@ -11,7 +11,31 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        
+        NSMutableArray *numbers = [NSMutableArray array];
+        NSNumber  *myNumber;
+        int i;
+        
+        // Create an arry with the number 0 - 9
+        
+        for(i=0; i<10;++i)
+        {
+            myNumber = [NSNumber numberWithInt:i];
+            [numbers addObject:myNumber];
+            
+            
+        }
+        
+        // Sequence through the arry and display the values
+        for (i=0; i<10; ++i) {
+            myNumber = [numbers objectAtIndex:i];
+            NSLog(@"%@",myNumber);
+        }
+        
+        //Look how NSLog can display it with a single %@ format
+        NSLog(@"============== Uisng a single NSLog");
+        NSLog(@"%@", numbers);
     }
     return 0;
 }
